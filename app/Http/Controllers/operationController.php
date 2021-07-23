4ade9927cba3a2e085106ad0a3cae1805e31aac2
issqTK <87451167+issqTK@users.noreply.github.com>
2021-07-23 22:52:15 +0100

    some changes

--- app/Http/Controllers/operationController.php
+++ app/Http/Controllers/operationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\pharmaciesItineraire;
 
 class operationController extends Controller
 {
@@ -28,16 +29,47 @@ class operationController extends Controller
         
     }
 
-    function addToPharmacie() {
+    function filterCity($city, $city_id) {
         $data = $this->csvToArray('datas.csv');
         $results = array();
+        
+        $j = 0;
         for($i = 0; $i < count($data); $i++) {
-            if(!empty($data[$i]['Phone']) ) {
-                echo '<pre>';        
-                print_r($data[$i]);
-                echo '</pre>';
+            if(!empty($data[$i]['Phone']) && preg_match("/{$city}/i", $data[$i]['City']) ) {
+               $results[$j]['phone'] = '0'.trim(str_replace(' ', '', str_replace('-', '', str_replace('+212', '', $data[$i]['Phone']))));
+               $results[$j]['gmaps_url'] = $data[$i]['Gmaps_URL'];
+               $results[$j]['lat'] = str_replace(',', '.', $data[$i]['Lat']);
+               $results[$j]['long'] = str_replace(',', '.', $data[$i]['Lng']);
+               $results[$j]['city_id'] = $city_id;
+               $j++;
             }
         }
 
+        return $results;
+    }
+
+    function addToPharmacie() {
+        $casablanca = $this->filterCity('rabat', 2);
+        $count = 0; 
+        $fails = 0;
+
+        for($i = 0; $i<count($casablanca); $i++) {
+            $phone = pharmaciesItineraire::where('phone', '=', $casablanca[$i]['phone'])->first();
+            if(!$phone){
+                 $data = pharmaciesItineraire::create([
+                    'phone' => $casablanca[$i]['phone'],
+                    'gmaps_url' => $casablanca[$i]['gmaps_url'],
+                    'lat' => $casablanca[$i]['lat'],
+                    'long' => $casablanca[$i]['long'],
+                    'city_id' => $casablanca[$i]['city_id'],
+                ]); 
+
+                $count++;
+                
+            } else {$fails++;}
+           
+        }
+        echo 'add '. $count;
+        echo 'fails '. $fails;
     }
 }
